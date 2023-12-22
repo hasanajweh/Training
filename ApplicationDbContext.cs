387b99b6275d0d909e0a93de2adaaf9e040950e0
Hasan Ajweh <152981163+hasanajweh@users.noreply.github.com>
2023-12-22 04:40:35 +0200

    ApplicationDbContext

--- ApplicationDbContext.cs
+++ ApplicationDbContext.cs
@@ -9,3 +9,20 @@ public class ApplicationDbContext : DbContext
 
     public DbSet<Customer> Customers { get; set; }
    }
+protected override void OnModelCreating(ModelBuilder modelBuilder)
+{
+    base.OnModelCreating(modelBuilder);
+
+    // one-to-many relationship between Customer and SupportTickets
+    modelBuilder.Entity<Customer>()
+        .HasMany(c => c.SupportTickets)
+        .WithOne(t => t.Customer)
+        .HasForeignKey(t => t.CustomerId);
+
+    // relationship between Service and SupportTickets
+    modelBuilder.Entity<Service>()
+        .HasMany(s => s.SupportTickets)
+        .WithOne(t => t.Service)
+        .HasForeignKey(t => t.ServiceId)
+        .IsRequired(false);
+}
