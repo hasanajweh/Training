e60a3a5f4d9430cd06ec37c1d786430f3323e8a5
Hasan Ajweh <152981163+hasanajweh@users.noreply.github.com>
2023-12-22 04:12:31 +0200

    ApplicationDbContext

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Customer> Customers { get; set; }
+   }
